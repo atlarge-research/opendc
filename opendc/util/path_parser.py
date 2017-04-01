c5671ab2e5115ce9c022a97a088300dc408e2aa4
leonoverweel <l.overweel@gmail.com>
2017-04-01 18:25:05 +0200

    Make path parser robust to trailing /

--- opendc/util/path_parser.py
+++ opendc/util/path_parser.py
@@ -3,19 +3,18 @@ import sys
 import re
 
 def parse(version, endpoint_path):
-    """Map an HTTP call to an API path"""
+    """Map an HTTP endpoint path to an API path"""
 
     with open('opendc/api/{}/paths.json'.format(version)) as paths_file:
         paths = json.load(paths_file)
     
-    endpoint_path_parts = endpoint_path.split('/')
+    endpoint_path_parts = endpoint_path.strip('/').split('/')
     paths_parts = [x.split('/') for x in paths if len(x.split('/')) == len(endpoint_path_parts)]
 
     for path_parts in paths_parts:
         found = True
 
         for (endpoint_part, part) in zip(endpoint_path_parts, path_parts):
-            print endpoint_part, part
             if not part.startswith('{') and endpoint_part != part:
                 found = False
                 break
