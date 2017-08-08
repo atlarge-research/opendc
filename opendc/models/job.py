958d67efbe6724d5b1b4cd56347e44464bc74d7d
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-08-08 12:04:45 +0300

    Add job to models

---
+++ opendc/models/job.py
@@ -0,0 +1,19 @@
+from opendc.models.model import Model
+
+class Job(Model):
+
+    JSON_TO_PYTHON_DICT = {
+        'Job': {
+            'id': 'id',
+            'name': 'name'
+        }
+    }
+
+    TABLE_NAME = 'jobs'
+    COLUMNS = ['id', 'name']
+    COLUMNS_PRIMARY_KEY = ['id']
+
+    def google_id_has_at_least(self, google_id, authorization_level):
+        """Return True if the user has at least the given auth level over this Job."""
+
+        return authorization_level not in ['EDIT', 'OWN']
