bf1117effbc1fc4769e10b1fa00f6116202d0c7f
Sacheendra Talluri <sacheendra.t@gmail.com>
2017-09-25 02:01:24 +0200

    Copy mysql environment variables into keys.json

--- build/configure.sh
+++ build/configure.sh
@@ -15,15 +15,25 @@ fi
 
 MYSQL_COMMAND="mysql -h mariadb -u $MYSQL_USER --password=$MYSQL_PASSWORD"
 
-until eval $MYSQL_COMMAND -e ";" ; do
+until eval $MYSQL_COMMAND -e "use opendc;" ; do
   echo "MariaDB is unavailable - sleeping"
   sleep 1
 done
 
-NUM_TABLES=$(eval "$MYSQL_COMMAND -B --disable-column-names \"SELECT count(*) FROM information_schema.tables WHERE table_schema='$MYSQL_DATABASE';\"")
+NUM_TABLES=$(eval "$MYSQL_COMMAND -B --disable-column-names -e \"SELECT count(*) FROM information_schema.tables WHERE table_schema='$MYSQL_DATABASE';\"")
 
 # Check if database is empty
 if [ "$NUM_TABLES" -eq 0 ]; then
 	eval $MYSQL_COMMAND "$MYSQL_DATABASE" < ./database/schema.sql
 	eval $MYSQL_COMMAND "$MYSQL_DATABASE" < ./database/test.sql
 fi
+
+# Writing databse config values to keys.json
+cat keys.json | python -c "import os, sys, json; ks = json.load(sys.stdin); \
+    ks['MYSQL_HOST'] = 'mariadb'; \
+    ks['MYSQL_PORT'] = '3306'; \
+    ks['MYSQL_DATABASE'] = os.environ['MYSQL_DATABASE']; \
+    ks['MYSQL_USER'] = os.environ['MYSQL_USER']; \
+    ks['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']; \
+    print json.dumps(ks, indent=4)" > new_keys.json
+mv new_keys.json keys.json
