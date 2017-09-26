83518ce800aba586b7591959f232a7ddfe4f8a9f
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-09-26 21:29:11 +0200

    Add logo to jumbotron

--- src/components/home/JumbotronHeader.js
+++ src/components/home/JumbotronHeader.js
@@ -4,11 +4,12 @@ import "./JumbotronHeader.css";
 const JumbotronHeader = () => (
     <section className="jumbotron-header">
         <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-3">Open<span className="dc">DC</span></h1>
+            <div className="jumbotron text-center">
+                <h1>Open<span className="dc">DC</span></h1>
                 <p className="lead">
                     Collaborative Datacenter Simulation and Exploration for Everybody
                 </p>
+                <img src="img/logo.png" className="img-responsive mt-3" alt="OpenDC"/>
             </div>
         </div>
     </section>
