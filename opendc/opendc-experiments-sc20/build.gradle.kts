52755a9cd839116888016b9277e6812a0f14d1d2
Georgios Andreadis <info@gandreadis.com>
2020-03-11 16:31:19 +0100

    Fix main file in gradle setup

--- opendc/opendc-experiments-sc20/build.gradle.kts
+++ opendc/opendc-experiments-sc20/build.gradle.kts
@@ -31,7 +31,7 @@ plugins {
 }
 
 application {
-    mainClassName = "com.atlarge.opendc.experiments.sc20.TestExperiment"
+    mainClassName = "com.atlarge.opendc.experiments.sc20.TestExperimentKt"
 }
 
 dependencies {
