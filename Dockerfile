0e01f7118fb265f6f6558fd9c63eb516581c1bee
jc0b <j.burley@vu.nl>
2020-06-09 14:19:54 +0200

    progress so far (for remote)

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
 FROM node:14.2.0
 MAINTAINER Sacheendra Talluri <sacheendra.t@gmail.com>
 
+# Adding the mongodb repo and installing the client
+RUN wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add - \
+	&& echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list \
+	&& apt-get update \
+	&& apt-get install -y mongodb-org
+
 # Installing python and web-server dependencies
 RUN echo "deb http://ftp.debian.org/debian stretch main" >> /etc/apt/sources.list \
 	&& apt-get update \
-	&& apt-get install -y python python-pip yarn git sed mysql-client \
+	&& apt-get install -y python3 python3-pip yarn git sed mysql-client pymongo \
+	&& pip3 install oauth2client eventlet flask-socketio flask-compress mysql-connector-python-rf \
+	&& pip3 install --upgrade pyasn1-modules \
 	&& rm -rf /var/lib/apt/lists/*
 
 # Copy OpenDC directory
@@ -24,4 +32,4 @@ RUN pip install -e /opendc/opendc-web-server \
 # Set working directory
 WORKDIR /opendc
 
-CMD ["sh", "-c", "./build/configure.sh && python2.7 opendc-web-server/main.py keys.json"]
+CMD ["sh", "-c", "./build/configure.sh && python3 opendc-web-server/main.py keys.json"]
--- build/configure.sh
+++ build/configure.sh
@@ -22,20 +22,20 @@ done
 
 echo "MariaDB available"
 
-NUM_TABLES=$(eval "$MYSQL_COMMAND -B --disable-column-names -e \"SELECT count(*) FROM information_schema.tables WHERE table_schema='$MYSQL_DATABASE';\"")
+#NUM_TABLES=$(eval "$MYSQL_COMMAND -B --disable-column-names -e \"SELECT count(*) FROM information_schema.tables WHERE table_schema='$MYSQL_DATABASE';\"")
 
 # Check if database is empty
-if [ "$NUM_TABLES" -eq 0 ]; then
-	eval $MYSQL_COMMAND "$MYSQL_DATABASE" < ./database/schema.sql
-	eval $MYSQL_COMMAND "$MYSQL_DATABASE" < ./database/test.sql
-fi
+#if [ "$NUM_TABLES" -eq 0 ]; then
+#	eval $MYSQL_COMMAND "$MYSQL_DATABASE" < ./database/schema.sql
+#	eval $MYSQL_COMMAND "$MYSQL_DATABASE" < ./database/test.sql
+#fi
 
 # Writing databse config values to keys.json
 cat keys.json | python -c "import os, sys, json; ks = json.load(sys.stdin); \
-    ks['MYSQL_HOST'] = 'mariadb'; \
-    ks['MYSQL_PORT'] = '3306'; \
-    ks['MYSQL_DATABASE'] = os.environ['MYSQL_DATABASE']; \
-    ks['MYSQL_USER'] = os.environ['MYSQL_USER']; \
-    ks['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']; \
+    ks['MONGODB_HOST'] = 'mongo'; \
+    ks['MONGODB_PORT'] = '27017'; \
+    ks['MONGODB_DATABASE'] = os.environ['MONGO_DB']; \
+    ks['MYSQL_USER'] = os.environ['MONGO_DB_USER']; \
+    ks['MYSQL_PASSWORD'] = os.environ['MONGO_DB_PASSWORD']; \
     print json.dumps(ks, indent=4)" > new_keys.json
 mv new_keys.json keys.json
