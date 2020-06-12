6f6c6fd0634dbe65f77aedcf77df88a7fa57c5fb
jc0b <j.burley@vu.nl>
2020-06-12 14:58:29 +0200

    refactored configure script and docker-compose for mongo

--- build/configure.sh
+++ build/configure.sh
@@ -1,26 +1,28 @@
-if [ -z "$MYSQL_DATABASE" ]; then
-    echo "MYSQL_DATABASE environment variable not specified"
+if [ -z "$MONGO_DB" ]; then
+    echo "MONGO_DB environment variable not specified"
     exit 1
 fi
 
-if [ -z "$MYSQL_USER" ]; then
-    echo "MYSQL_USER environment variable not specified"
+if [ -z "$MONGO_DB_USER" ]; then
+    echo "MONGO_DB_USER environment variable not specified"
     exit 1
 fi
 
-if [ -z "$MYSQL_PASSWORD" ]; then
-    echo "MYSQL_PASSWORD environment variable not specified"
+if [ -z "$MONGO_DB_PASSWORD" ]; then
+    echo "MONGO_DB_PASSWORD environment variable not specified"
     exit 1
 fi
 
-MYSQL_COMMAND="mysql -h mariadb -u $MYSQL_USER --password=$MYSQL_PASSWORD"
+#MYSQL_COMMAND="mysql -h mariadb -u $MYSQL_USER --password=$MYSQL_PASSWORD"
 
-until eval $MYSQL_COMMAND -e "use opendc;" ; do
-  echo "MariaDB is unavailable - sleeping"
+MONGO_COMMAND="mongo $MONGO_DB -h $MONGO_DB_HOST --port $MONGO_DB_PORT -u $MONGO_DB_USERNAME -p $MONGO_DB_PASSWORD --authenticationDatabase $MONGO_DB"
+
+until eval $MONGO_COMMAND --eval 'db.getCollectionNames();' ; do
+  echo "MongoDB is unavailable - sleeping"
   sleep 1
 done
 
-echo "MariaDB available"
+echo "MongoDB available"
 
 #NUM_TABLES=$(eval "$MYSQL_COMMAND -B --disable-column-names -e \"SELECT count(*) FROM information_schema.tables WHERE table_schema='$MYSQL_DATABASE';\"")
 
@@ -32,8 +34,8 @@ echo "MariaDB available"
 
 # Writing databse config values to keys.json
 cat keys.json | python -c "import os, sys, json; ks = json.load(sys.stdin); \
-    ks['MONGODB_HOST'] = 'mongo'; \
-    ks['MONGODB_PORT'] = '27017'; \
+    ks['MONGODB_HOST'] = os.environ['MONGO_DB_HOST']; \
+    ks['MONGODB_PORT'] = os.environ['MONGO_DB_PORT']; \
     ks['MONGODB_DATABASE'] = os.environ['MONGO_DB']; \
     ks['MYSQL_USER'] = os.environ['MONGO_DB_USER']; \
     ks['MYSQL_PASSWORD'] = os.environ['MONGO_DB_PASSWORD']; \
