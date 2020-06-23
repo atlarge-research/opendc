239173c17b0cd314a3d515cfa46bccfec05402c2
Georgios Andreadis <info@gandreadis.com>
2020-06-23 14:19:54 +0200

    Add all collections

--- mongodb/mongo-init-opendc-db.sh
+++ mongodb/mongo-init-opendc-db.sh
@@ -4,20 +4,36 @@ echo 'Creating opendc user and db'
 
 mongo opendc --host localhost \
         --port 27017 \
-        -u $MONGO_INITDB_ROOT_USERNAME \
-        -p $MONGO_INITDB_ROOT_PASSWORD \
+        -u "$MONGO_INITDB_ROOT_USERNAME" \
+        -p "$MONGO_INITDB_ROOT_PASSWORD" \
         --authenticationDatabase admin \
         --eval "db.createUser({user: '$OPENDC_DB_USERNAME', pwd: '$OPENDC_DB_PASSWORD', roles:[{role:'dbOwner', db: '$OPENDC_DB'}]});"
-MONGO_ROOT_CMD="mongo $OPENDC_DB --host localhost --port 27017 -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin"
 
-#echo 'Creating opendc db schema...'
 MONGO_CMD="mongo $OPENDC_DB -u $OPENDC_DB_USERNAME -p $OPENDC_DB_PASSWORD --authenticationDatabase $OPENDC_DB"
+
+echo 'Creating collections'
+
+$MONGO_CMD --eval 'db.createCollection("users");'
+$MONGO_CMD --eval 'db.createCollection("simulations");'
+$MONGO_CMD --eval 'db.createCollection("topologies");'
+$MONGO_CMD --eval 'db.createCollection("experiments");'
 $MONGO_CMD --eval 'db.createCollection("prefabs");'
+
+echo 'Loading test data'
+
+$MONGO_CMD --eval 'db.users.insertOne(
+    {
+        "googleId": "23483578932789231",
+        "email": "jorgos.andreadis@gmail.com",
+        "givenName": "Jorgos",
+        "familyName": "Andreadis",
+        "authorizations": []
+    });'
 
 $MONGO_CMD --eval 'db.prefabs.insertOne(
     {
         "type": "rack",
-        "name": "test_rack3",
+        "name": "testRack3",
         "size": 42,
         "depth": 42,
         "author": "Jacob Burley",
@@ -26,7 +42,7 @@ $MONGO_CMD --eval 'db.prefabs.insertOne(
             {
                 "type": "switch",
                 "ports": 48,
-                "power_draw": 150,
+                "powerDraw": 150,
                 "psus": 1,
                 "size": 1
             },
@@ -37,16 +53,16 @@ $MONGO_CMD --eval 'db.prefabs.insertOne(
                     {
                         "type": "mainboard",
                         "sockets": 1,
-                        "dimm_slots": 4,
+                        "dimmSlots": 4,
                         "nics": 1,
-                        "pcie_slots": 2,
+                        "pcieSlots": 2,
                         "children": [
                             {
                                 "type": "CPU",
                                 "coreCount": 4,
                                 "SMT": true,
-                                "base_clk": 3.5,
-                                "boost_clk": 3.9,
+                                "baseClk": 3.5,
+                                "boostClk": 3.9,
                                 "brand": "Intel",
                                 "SKU": "i7-3770K",
                                 "socket": "LGA1155",
@@ -82,7 +98,7 @@ $MONGO_CMD --eval 'db.prefabs.insertOne(
                                 "coreCount": 2304,
                                 "brand": "AMD",
                                 "technologies": "OpenCL",
-                                "pcie_gen": "3x16",
+                                "pcieGen": "3x16",
                                 "tdp": 169,
                                 "slots": 2
                             }
@@ -98,35 +114,9 @@ $MONGO_CMD --eval 'db.prefabs.insertOne(
                         "size": 2000,
                         "interface": "SATA",
                         "media": "flash",
-                        "form_factor": 2.5
+                        "formFactor": 2.5
                     }
                 ]
-            }
-        ]
-    });'
-
-$MONGO_CMD --eval 'db.createCollection("topologies");'
-
-$MONGO_CMD --eval 'db.createCollection("users");'
-
-$MONGO_CMD --eval 'db.users.insertOne(
-    {
-        "google_id": "23483578932789231",
-        "email": "jorgos.andreadis@gmail.com",
-        "given_name": "Jorgos",
-        "family_name": "Andreadis",
-        "authorizations": [
-            {
-                "simulation_id": 1,
-                "authorization_level": "OWN"
-            },
-            {
-                "simulation_id": 2,
-                "authorization_level": "READ"
-            },
-            {
-                "simulation_id": 3,
-                "authorization_level": "READWRITE"
             }
         ]
     });'
