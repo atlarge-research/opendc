40185f6ddc9c4620c62273366a0fb491ecf2cad3
Georgios Andreadis <info@gandreadis.com>
2020-06-26 14:24:07 +0200

    Implement scheduler endpoints

--- opendc/api/v2/schedulers/endpoint.py
+++ opendc/api/v2/schedulers/endpoint.py
@@ -1,14 +1,10 @@
-from opendc.models_old.scheduler import Scheduler
 from opendc.util.rest import Response
 
 
-def GET(request):
+SCHEDULERS = ['DEFAULT']
+
+
+def GET(_):
     """Get all available Schedulers."""
 
-    # Get the Schedulers
-
-    schedulers = Scheduler.query()
-
-    # Return the Schedulers
-
-    return Response(200, 'Successfully retrieved Schedulers.', [x.to_JSON() for x in schedulers])
+    return Response(200, 'Successfully retrieved Schedulers.', [{'name': name} for name in SCHEDULERS])
