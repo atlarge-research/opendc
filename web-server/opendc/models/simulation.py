690818051d0c9768cdaf735acf77ea9e98f00b38
Georgios Andreadis <info@gandreadis.com>
2020-06-30 10:31:27 +0200

    Implement authorizations endpoint

--- web-server/opendc/models/simulation.py
+++ web-server/opendc/models/simulation.py
@@ -1,5 +1,6 @@
 from opendc.models.model import Model
 from opendc.models.user import User
+from opendc.util.database import DB
 from opendc.util.exceptions import ClientError
 from opendc.util.rest import Response
 
@@ -13,3 +14,10 @@ class Simulation(Model):
             filter(lambda x: str(x['simulationId']) == str(self.obj['_id']), user.obj['authorizations']))
         if len(authorizations) == 0 or (edit_access and authorizations[0]['authorizationLevel'] == 'VIEW'):
             raise ClientError(Response(403, "Forbidden from retrieving simulation."))
+
+    def get_all_authorizations(self):
+        return [
+            user['_id'] for user in DB.fetch_all({'authorizations': {
+                'simulationId': self.obj['_id']
+            }}, User.collection_name)
+        ]
