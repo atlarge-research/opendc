5750e4f4eb360ecda39b1568d903e71ae09e141f
Fabian Mastenbroek <mail.fabianm@gmail.com>
2020-11-10 22:32:21 +0100

    Add support for frontend error reporting via Sentry
    This change sets up Sentry to collect errors that occur at the frontend.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import { Provider } from 'react-redux'
 import { setupSocketConnection } from './api/socket'
 import './index.sass'
@@ -8,6 +10,16 @@ import configureStore from './store/configure-store'
 
 setupSocketConnection(() => {
     const store = configureStore()
+
+    // Initialize Sentry if the user has configured a DSN
+    if (process.env.REACT_APP_SENTRY_DSN) {
+        Sentry.init({
+            environment: process.env.NODE_ENV,
+            dsn: process.env.REACT_APP_SENTRY_DSN,
+            integrations: [new Integrations.BrowserTracing()],
+            tracesSampleRate: 0.5,
+        })
+    }
 
     ReactDOM.render(
         <Provider store={store}>
