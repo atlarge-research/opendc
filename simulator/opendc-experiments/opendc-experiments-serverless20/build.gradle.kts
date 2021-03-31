c2250720d694c6e7e19b3c0ba2fc27a124d3cadb
Fabian Mastenbroek <mail.fabianm@gmail.com>
2021-03-31 14:21:19 +0200

    exp: Add trace reader for Serverless experiments
    This change adds the trace reader for the serverless experiments as
    described in #48.

---
+++ simulator/opendc-experiments/opendc-experiments-serverless20/build.gradle.kts
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 AtLarge Research
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+description = "Experiments for OpenDC Serverless"
+
+/* Build configuration */
+plugins {
+    `kotlin-library-conventions`
+    `experiment-conventions`
+    `testing-conventions`
+}
+
+dependencies {
+    api(platform(project(":opendc-platform")))
+    api(project(":opendc-harness"))
+    implementation(project(":opendc-serverless:opendc-serverless-service"))
+    implementation(project(":opendc-serverless:opendc-serverless-simulator"))
+    implementation(project(":opendc-telemetry:opendc-telemetry-sdk"))
+
+    implementation("io.github.microutils:kotlin-logging")
+}
