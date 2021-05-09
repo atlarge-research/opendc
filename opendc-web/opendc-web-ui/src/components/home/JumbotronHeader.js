a0345ef9aac2138b6b78cdf72a168862e47a45da
Fabian Mastenbroek <mail.fabianm@gmail.com>
2021-05-09 11:21:51 +0200

    ui: Link to OpenDC 2.0 paper

--- opendc-web/opendc-web-ui/src/components/home/JumbotronHeader.js
+++ opendc-web/opendc-web-ui/src/components/home/JumbotronHeader.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import { Container, Jumbotron, Button } from 'reactstrap'
 import './JumbotronHeader.sass'
 
 const JumbotronHeader = () => (
     <section className="jumbotron-header">
-        <div className="container">
-            <div className="jumbotron text-center">
+        <Container>
+            <Jumbotron className="text-center">
                 <h1>
                     Open<span className="dc">DC</span>
                 </h1>
                 <p className="lead">Collaborative Datacenter Simulation and Exploration for Everybody</p>
                 <img src="img/logo.png" className="img-responsive mt-3" alt="OpenDC" />
-            </div>
-        </div>
+                <p className="lead mt-5">
+                    <Button
+                        tag="a"
+                        target="_blank"
+                        href="https://atlarge-research.com/pdfs/ccgrid21-opendc-paper.pdf"
+                        color="warning"
+                    >
+                        Read about <strong>OpenDC 2.0</strong> <i className="fa fa-external-link" />
+                    </Button>
+                </p>
+            </Jumbotron>
+        </Container>
     </section>
 )
 
