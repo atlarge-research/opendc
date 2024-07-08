aceb566eeed719031d3539fa51f0e8df87fd24da
Radu Nicolae <rnicolae04@gmail.com>
2024-07-08 11:36:40 +0200

    (feat) user-input parser file

--- opendc-analyze/src/main/python/input_parser.py
+++ opendc-analyze/src/main/python/input_parser.py
@@ -1,6 +1,36 @@
 import os
+import json
+import utils
+
 
 def read_input(path=""):
-    print("The current path is " + os.getcwd())
-    print("Input read")
+    if (path == ""):
+        os.exit(1918)
+
+    # Remove any trailing commas or spaces
+    path = path.strip().strip(',')
+
+    # Ensure the path is relative to the project root
+    project_root = find_root_dir()
+    full_path = os.path.join(project_root, path)
+
+    # Read the JSON file from the given path
+    with open(full_path) as raw_json:
+        parsed_json = json.load(raw_json)
+
+    switch_to_root_dir()
+    return parsed_json
+
+
+def find_root_dir():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    while current_dir != os.path.dirname(current_dir):  # while not at the filesystem root
+        if os.path.exists(os.path.join(current_dir, 'README.md')):  # or any other root-specific file or directory
+            return current_dir
+        current_dir = os.path.dirname(current_dir)
+
+
+def switch_to_root_dir():
+    root_dir = find_root_dir()
+    os.chdir(root_dir)
 
