2fa2438f37c497c0ddcf5f3d5bcdaa1820b62afc
Radu Nicolae <rnicolae04@gmail.com>
2024-07-09 14:24:31 +0200

    order of the arguments repaired

--- opendc-analyze/src/main/python/main.py
+++ opendc-analyze/src/main/python/main.py
@@ -9,8 +9,8 @@ from models.MultiModel import MultiModel
 from input_parser import read_input
 
 
-# e.g., argument 1 -- experiments/experiment-2-window-performance-analysis/inputs/analyzer.json
-# e.g., argument 2 -- experiments/experiment-2-window-performance-analysis/outputs//window_size_performance_analysis
+# e.g., argument 1 -- experiments/experiment-2-window-performance-analysis/outputs//window_size_performance_analysis
+# e.g., argument 2 -- experiments/experiment-2-window-performance-analysis/inputs/analyzer.json
 
 # parameter input for main
 # "experiments/experiment-2-window-performance-analysis/inputs/analyzer.json", "experiments/experiment-2-window-performance-analysis/outputs/window-size-performance-analysis"
@@ -19,7 +19,7 @@ def main():
     print("ARGV1 is " + sys.argv[1])
     print("ARGV2 is " + sys.argv[2])
     print("The current path is " + os.getcwd())
-    user_input = read_input(sys.argv[1])
+    user_input = read_input(sys.argv[2])
     # comm
     # if user wants to run a multi-model
     if (user_input["multimodel"]):
