f13c6d7862734a44ec48f903895041392f19fa52
Radu Nicolae <rnicolae04@gmail.com>
2024-07-16 14:59:13 +0200

    (feat) MetaModel very-basic functionality
    initialization, computation with only one function (mean), multi-model appending and linking, plotting tested (and works) fot time series graph yet

--- opendc-analyze/src/main/python/models/MetaModel.py
+++ opendc-analyze/src/main/python/models/MetaModel.py
@@ -6,81 +6,54 @@ import numpy as np
 import utils
 from .Model import Model
 
-"""
-The MetaModel class is used to compute / aggregate a new model, by using the MultiModel class. The MetaModel takes
-data from MultiModel and computes a new model, at a certain granularity, using a function (e.g., median, mean).
-
-While mean succeeded in various sciences, we will further use median, as it is more robust to outliers. We will also
-further analyze, integrate, and evaluate the MetaModel against individual models and test various methodologies of
-aggregation (e.g., mathematical functions, machine learning models).
-"""
-
 
 class Metamodel:
     def __init__(self, multimodel):
-        self.multimodel = multimodel
-        self.models = multimodel.models
-        self.metric = multimodel.metric
-        self.measure_unit = multimodel.measure_unit
-        self.window_size = multimodel.window_size
-        self.metamodel_data = []
-        self.compute()  # compute the metamodel on initialization
+        if multimodel.user_input['metamodel']:
+            self.multimodel = multimodel
+            self.meta_model_entries = []
+            self.meta_model_function = multimodel.user_input['meta_simulation_function']
+            self.compute_metamodel()
+        else:
+            raise ValueError("Metamodel is not enabled in the config file")
 
-    """
-    The compute function takes each model and computes the mean of the chunks of the host data,
-    at a granularity defined by the window-size.
+    def compute_metamodel(self):
+        min_model_len = min([len(model.processed_host_data) for model in self.multimodel.models])
+        number_of_models = len(self.multimodel.models)
 
-    This function is called on initialization and computes the MetaModel data.
-    """
+        if self.multimodel.window_size == 1:
+            self.compute_metamodel_single_window(max_length, number_of_models)
+        else:
+            for i in range(0, min_model_len):
+                data_entries = []
+                for j in range(number_of_models):
+                    data_entries.append(self.multimodel.models[j].processed_host_data[i]) # iloc is the pandas equivalent of .at()
 
-    def compute(self):
-        for index in range(len(self.models[0].processed_host_data)):
-            array_at_index = []
-            for model in self.models:
-                if index + self.window_size < len(model.processed_host_data):
-                    array_at_index.append(model.processed_host_data[index])
+                self.meta_model_entries.append(self.mean(data_entries))
 
-            median_at_granularity = np.median(array_at_index)
-            self.metamodel_data.append(median_at_granularity)
+    def compute_metamodel_single_window(self, max_length, number_of_models):
+        for i in range(0, max_length):
+            data_entries = []
+            for j in range(number_of_models):
+                data_entries.append(self.multimodel.models[j].processed_host_data.iloc[i])
 
-    """
-    The generate function sets up the plot, plots the data and saves the plot.
-    """
+            self.meta_model_entries.append(self.mean(data_entries))
 
-    def generate(self):
-        self.setup_plot()
-        self.plot()
-        self.save_plot()
+    def mean(self, chunks):
+        return np.mean(chunks)
 
-    """
-    Set up the plot for the MetaModel.
-    """
-
-    def setup_plot(self):
-        plt.figure(figsize=(30, 10))
-        plt.title(self.metric)
-        plt.xlabel("Time [s]")
-        plt.ylabel(self.metric + self.measure_unit)
-        plt.ylim(0, self.multimodel.set_y_axis_lim())
-
-        # multiply all the values from the x-axis by the window size
-        plt.xticks(
-            np.arange(0, len(self.metamodel_data), step=self.window_size),
-            np.arange(0, len(self.metamodel_data) * self.window_size, step=100 * self.window_size)
+    def append_to_multi_model(self):
+        meta_model = Model(
+            raw_host_data=self.meta_model_entries,
+            id=101,
+            path=self.multimodel.output_folder_path
         )
-        plt.grid()
 
-    """
-    Plot the processed MetaModel data.
-    """
+        size_of_meta_model = len(self.meta_model_entries)
+        size_of_model = len(self.multimodel.models[0].processed_host_data)
+        meta_model.processed_host_data = self.meta_model_entries
+        self.multimodel.models.append(meta_model)
 
-    def plot(self):
-        plt.plot(self.metamodel_data)
-
-    """
-    Save the plot in the analysis folder.
-    """
-
-    def save_plot(self):
-        folder_prefix = "./" + utils.SIMULATION_ANALYSIS_FOLDER_NAME + "/" + self.metric + "/"
-        plt.savefig(folder_prefix + "metamodel_metric=" + self.metric + "_window=" + str(self.window_size) + ".png")
+    def plot_metamodel(self):
+        self.append_to_multi_model()
+        self.multimodel.generate_plot()
