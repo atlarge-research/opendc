db13f9552a9e80825f9fd74cb50dabf700aaca0d
Radu Nicolae <rnicolae04@gmail.com>
2024-07-17 17:03:16 +0200

    (feat) last commit before major refactoring

--- opendc-analyze/src/main/python/models/MetaModel.py
+++ opendc-analyze/src/main/python/models/MetaModel.py
@@ -13,49 +13,75 @@ class Metamodel:
     def __init__(self, multimodel):
         if multimodel.user_input['metamodel']:
             self.multimodel = multimodel
+            self.metamodel = Model(
+                raw_host_data=pd.Series(),
+                id=META_MODEL_ID,
+                path=self.multimodel.output_folder_path
+            )
+
+
             self.meta_model_entries = []
             self.meta_model_function = multimodel.user_input['meta_simulation_function']
+            self.min_raw_model_len = min([len(model.raw_host_data) for model in self.multimodel.models])
+            self.min_processed_model_len = min([len(model.processed_host_data) for model in self.multimodel.models])
+            self.number_of_models = len(self.multimodel.models)
             self.meta_cumulative = 0
-            self.cumulative_time_series = 0
-            self.compute_metamodel()
+            self.compute()
+
         else:
             raise ValueError("Metamodel is not enabled in the config file")
 
-    def compute_metamodel(self):
-        min_processed_model_len = min([len(model.processed_host_data) for model in self.multimodel.models])
-        min_raw_model_len = min([len(model.raw_host_data) for model in self.multimodel.models])
-        number_of_models = len(self.multimodel.models)
+    def compute(self):
+        if self.multimodel.user_input['plot_type'] == 'time_series':
+            self.compute_time_series()
 
-        if self.multimodel.user_input['plot_type'] == 'cumulative':
-            for i in range(0, min_raw_model_len):
-                data_entries = []
-                for j in range(number_of_models):
-                    host_data = self.multimodel.models[j].raw_host_data
-                    ith_element = host_data.iloc[i]
-                    data_entries.append(ith_element)
-                self.meta_cumulative += self.mean(data_entries)
-        elif self.multimodel.user_input['plot_type'] == 'time_series':
-            if self.multimodel.window_size == 1:
-                self.compute_for_window_1(min_processed_model_len, number_of_models)
-            else:
-                self.compute_for_any_window(min_processed_model_len, number_of_models)
+        elif self.multimodel.user_input['plot_type'] == 'cumulative':
+            self.compute_cumulative()
 
         elif self.multimodel.user_input['plot_type'] == 'cumulative_time_series':
-            pass
+            self.compute_cumulative_time_series()
 
-    def compute_for_window_1(self, min_processed_model_len, number_of_models):
-        for i in range(0, min_processed_model_len):
+
+    def compute_cumulative(self):
+        for i in range(0, self.min_raw_model_len):
             data_entries = []
-            for j in range(number_of_models):
+            for j in range(self.number_of_models):
+                host_data = self.multimodel.models[j].raw_host_data
+                ith_element = host_data.iloc[i]
+                data_entries.append(ith_element)
+            self.meta_cumulative += self.mean(data_entries)
+
+    def compute_time_series(self):
+        if self.multimodel.window_size == 1:
+            self.compute_for_window_1()
+        else:
+            self.compute_for_any_window()
+
+    def compute_cumulative_time_series(self):
+        sum = 0
+        self.meta_model_entries.append(sum)
+        for i in range(0, self.min_raw_model_len):
+            data_entries = []
+            for j in range(self.number_of_models):
+                host_data = self.multimodel.models[j].raw_host_data
+                ith_element = host_data.iloc[i]
+                data_entries.append(ith_element)
+            sum += self.mean(data_entries)
+            self.meta_model_entries.append(sum)
+
+    def compute_for_window_1(self):
+        for i in range(0, self.min_processed_model_len):
+            data_entries = []
+            for j in range(self.number_of_models):
                 host_data = self.multimodel.models[j].raw_host_data
                 ith_element = host_data.iloc[i]
                 data_entries.append(ith_element)
             self.meta_model_entries.append(self.mean(data_entries))
 
-    def compute_for_any_window(self, min_processed_model_len, number_of_models):
-        for i in range(0, min_processed_model_len):
+    def compute_for_any_window(self):
+        for i in range(0, self.min_processed_model_len):
             data_entries = []
-            for j in range(number_of_models):
+            for j in range(self.number_of_models):
                 host_data = self.multimodel.models[j].processed_host_data
                 ith_element = host_data[i]
                 data_entries.append(ith_element)
@@ -79,5 +105,20 @@ class Metamodel:
         self.multimodel.models.append(meta_model)
 
     def plot_metamodel(self):
+        if self.multimodel.plot_type == 'cumulative_time_series':
+            self.plot_cumulative_time_series()
+        else:
+            self.append_to_multi_model()
+            self.multimodel.generate_plot()
+
+
+    def plot_cumulative_time_series(self):
+        meta_model = Model(
+            raw_host_data=pd.Series(self.meta_model_entries),
+            id=META_MODEL_ID,
+            path=self.multimodel.output_folder_path
+        )
+
+        self.multimodel.models.append(meta_model)
         self.append_to_multi_model()
         self.multimodel.generate_plot()
