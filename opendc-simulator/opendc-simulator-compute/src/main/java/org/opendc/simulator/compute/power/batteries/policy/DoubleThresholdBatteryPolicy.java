e9de39306049ba33ebd852b6d58aa19017bcbe12
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2025-01-31 16:10:10 +0100

    Spotless applied

--- opendc-simulator/opendc-simulator-compute/src/main/java/org/opendc/simulator/compute/power/batteries/policy/DoubleThresholdBatteryPolicy.java
+++ opendc-simulator/opendc-simulator-compute/src/main/java/org/opendc/simulator/compute/power/batteries/policy/DoubleThresholdBatteryPolicy.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2025 AtLarge Research
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.opendc.simulator.compute.power.batteries.policy;
 
 import org.opendc.simulator.compute.power.batteries.BatteryAggregator;
@@ -17,7 +39,7 @@ public class DoubleThresholdBatteryPolicy extends BatteryPolicy {
      * @param carbonThreshold
      */
     public DoubleThresholdBatteryPolicy(
-        FlowGraph parentGraph, SimBattery battery, BatteryAggregator aggregator, double carbonThreshold) {
+            FlowGraph parentGraph, SimBattery battery, BatteryAggregator aggregator, double carbonThreshold) {
         super(parentGraph, battery, aggregator);
 
         this.carbonThreshold = carbonThreshold;
@@ -39,5 +61,4 @@ public class DoubleThresholdBatteryPolicy extends BatteryPolicy {
         this.setBatteryState(BatteryState.Idle);
         return Long.MAX_VALUE;
     }
-
 }
