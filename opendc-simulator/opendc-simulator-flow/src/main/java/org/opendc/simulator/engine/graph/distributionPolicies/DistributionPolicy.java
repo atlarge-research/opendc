ac2362390d62663f43cec33d3f75f9e207bfc0c6
Niels Thiele <noleu66@posteo.net>
2025-06-20 22:35:15 +0200

    implemented support of multiple suppliers in flow distributor

--- opendc-simulator/opendc-simulator-flow/src/main/java/org/opendc/simulator/engine/graph/distributionPolicies/DistributionPolicy.java
+++ opendc-simulator/opendc-simulator-flow/src/main/java/org/opendc/simulator/engine/graph/distributionPolicies/DistributionPolicy.java
@@ -25,5 +25,5 @@ package org.opendc.simulator.engine.graph.distributionPolicies;
 import java.util.ArrayList;
 
 public interface DistributionPolicy {
-    double[] distributeSupply(ArrayList<Double> supply, double currentSupply);
+    double[] distributeSupply(ArrayList<Double> supply, ArrayList<Double> currentSupply, double totalSupply);
 }
