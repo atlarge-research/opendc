3feb66c9fc1934aa3137f09d5a27eb2cfea138f0
vincent <you@example.com>
2026-03-13 16:37:32 +0100

    minor fix

--- opendc-compute/opendc-compute-simulator/src/test/kotlin/org/opendc/compute/simulator/host/SimHostTelemetryTest.kt
+++ opendc-compute/opendc-compute-simulator/src/test/kotlin/org/opendc/compute/simulator/host/SimHostTelemetryTest.kt
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2026 AtLarge Research
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.opendc.compute.simulator.host
 
 import io.mockk.mockk
@@ -20,23 +42,31 @@ class SimHostTelemetryTest {
         val engine = mockk<FlowEngine>(relaxed = true)
         val cpuModel = CpuModel(0, 4, 2600.0, "vendor", "model", "arch")
         val memoryUnit = MemoryUnit("vendor", "model", 3200.0, 1024)
-        val machineModel = MachineModel(cpuModel, memoryUnit, null, FlowDistributorFactory.DistributionPolicy.MAX_MIN_FAIRNESS, FlowDistributorFactory.DistributionPolicy.MAX_MIN_FAIRNESS)
+        val machineModel =
+            MachineModel(
+                cpuModel,
+                memoryUnit,
+                null,
+                FlowDistributorFactory.DistributionPolicy.MAX_MIN_FAIRNESS,
+                FlowDistributorFactory.DistributionPolicy.MAX_MIN_FAIRNESS,
+            )
         val powerModel = mockk<PowerModel>(relaxed = true)
         val distributor = mockk<FlowDistributor>(relaxed = true)
 
-        val host = SimHost(
-            name = "H01",
-            type = "host",
-            clusterName = "C01",
-            clock = clock,
-            engine = engine,
-            machineModel = machineModel,
-            cpuPowerModel = powerModel,
-            gpuPowerModel = null,
-            embodiedCarbon = 0.0,
-            expectedLifetime = 0.0,
-            powerDistributor = distributor
-        )
+        val host =
+            SimHost(
+                name = "H01",
+                type = "host",
+                clusterName = "C01",
+                clock = clock,
+                engine = engine,
+                machineModel = machineModel,
+                cpuPowerModel = powerModel,
+                gpuPowerModel = null,
+                embodiedCarbon = 0.0,
+                expectedLifetime = 0.0,
+                powerDistributor = distributor,
+            )
 
         val task = mockk<ServiceTask>(relaxed = true)
         io.mockk.every { task.name } returns "T01"
