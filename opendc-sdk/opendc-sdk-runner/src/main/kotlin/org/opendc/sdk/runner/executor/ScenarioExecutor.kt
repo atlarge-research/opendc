b0fc050c3a81dba663261af953cdae62bf8ee43d
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2026-08-03 16:46:34 +0200

    removed a sleep

--- opendc-sdk/opendc-sdk-runner/src/main/kotlin/org/opendc/sdk/runner/executor/ScenarioExecutor.kt
+++ opendc-sdk/opendc-sdk-runner/src/main/kotlin/org/opendc/sdk/runner/executor/ScenarioExecutor.kt
@@ -95,9 +95,6 @@ private class ScenarioRun(
     suspend fun execute(): RunResult {
         val workload = scenario.workload.toServiceTasks(scenario.checkpointModel, resources::resolve)
 
-        println("Workload Loaded")
-        Thread.sleep(10_000)
-
         val startTime = workload.minOf { it.submittedAt }
         val clusters = scenario.topology.toClusterSpecs(resources::resolve)
 
