0539688e8e606f92e4b6f0cdd6fab9abf96a3d0c
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2026-09-12 13:04:39 +0200

    Separated the steps in HostsProvisioningStep.kt

--- opendc-sdk/opendc-sdk-runner/src/main/kotlin/org/opendc/sdk/runner/provision/HostsProvisioningStep.kt
+++ opendc-sdk/opendc-sdk-runner/src/main/kotlin/org/opendc/sdk/runner/provision/HostsProvisioningStep.kt
@@ -26,14 +26,17 @@ import org.opendc.common.ResourceType
 import org.opendc.compute.carbon.getCarbonFragments
 import org.opendc.compute.simulator.host.SimHost
 import org.opendc.compute.simulator.service.ComputeService
+import org.opendc.compute.topology.specs.BatteryJSONSpec
 import org.opendc.compute.topology.specs.ClusterSpec
 import org.opendc.compute.topology.specs.HostSpec
+import org.opendc.compute.topology.specs.PowerSourceSpec
 import org.opendc.compute.topology.specs.createSimBatteryPolicy
 import org.opendc.simulator.compute.carbon.CarbonModel
 import org.opendc.simulator.compute.power.SimPowerSource
 import org.opendc.simulator.compute.power.batteries.BatteryAggregator
 import org.opendc.simulator.compute.power.batteries.SimBattery
 import org.opendc.simulator.engine.engine.FlowEngine
+import org.opendc.simulator.engine.graph.FlowDistributor
 import org.opendc.simulator.engine.graph.FlowEdge
 import org.opendc.simulator.engine.graph.distributionPolicies.FlowDistributorFactory
 import org.opendc.simulator.engine.graph.distributionPolicies.FlowDistributorFactory.DistributionPolicy
@@ -50,107 +53,49 @@ public class HostsProvisioningStep(
     private val clusterSpecs: List<ClusterSpec>,
     private val startTime: Long = 0L,
 ) : ProvisioningStep {
+
+    private val simHosts = mutableSetOf<SimHost>()
+    private val simPowerSources = mutableListOf<SimPowerSource>()
+
     override fun apply(ctx: ProvisioningContext): AutoCloseable {
         val service =
             requireNotNull(
                 ctx.registry.resolve(serviceDomain, ComputeService::class.java),
             ) { "Compute service $serviceDomain does not exist" }
-        val simHosts = mutableSetOf<SimHost>()
-        val simPowerSources = mutableListOf<SimPowerSource>()
 
         val engine = FlowEngine.create(ctx.dispatcher)
 
-        for ((name, hostSpecs, powerSourceSpec, batterySpec) in clusterSpecs) {
+        for ((clusterName, hostSpecs, powerSourceSpec, batterySpec) in clusterSpecs) {
+
             // Create the Power Source to which hosts are connected
+            val (simPowerSource, powerDistributor) = this.createSimPowerSource(service, engine, powerSourceSpec, clusterName, hostSpecs.size)
 
-            // Create Power Source
-            val simPowerSource = SimPowerSource(engine, powerSourceSpec.totalPower.toDouble(), powerSourceSpec.name, name)
-            simPowerSources.add(simPowerSource)
-            service.addPowerSource(simPowerSource)
+            // Create the carbonmodel if provided
+            val carbonModel: CarbonModel? = createCarbonModel(ctx, engine, powerSourceSpec, simPowerSource)
 
-            val powerDistributor =
-                FlowDistributorFactory.getFlowDistributor(
-                    engine,
-                    DistributionPolicy.MAX_MIN_FAIRNESS,
-                    hostSpecs.size,
-                    1,
-                )
-
-            val carbonFragments = getCarbonFragments(powerSourceSpec.carbonTracePath)
-
-            var carbonModel: CarbonModel? = null
-            // Create Carbon Model
-            if (carbonFragments != null) {
-                carbonModel = CarbonModel(engine, carbonFragments, startTime)
-                carbonModel.addReceiver(simPowerSource)
-                ctx.registry.register(serviceDomain, CarbonModel::class.java, carbonModel)
-            }
-
-            if (batterySpec != null) {
-                // Create Battery Distributor
-                val batteryDistributor =
-                    FlowDistributorFactory.getFlowDistributor(
-                        engine,
-                        DistributionPolicy.MAX_MIN_FAIRNESS,
-                        2,
-                        1,
-                    )
-                FlowEdge(batteryDistributor, simPowerSource)
-
-                // Create Battery
-                val battery =
-                    SimBattery(
-                        engine,
-                        batterySpec.capacity,
-                        batterySpec.chargingSpeed,
-                        batterySpec.initialCharge,
-                        batterySpec.name,
-                        name,
-                        batterySpec.embodiedCarbon,
-                        batterySpec.expectedLifetime,
-                    )
-                FlowEdge(battery, batteryDistributor)
-
-                // Create Aggregator
-                val batteryAggregator = BatteryAggregator(engine, battery, batteryDistributor)
-
-                val batteryPolicy =
-                    createSimBatteryPolicy(
-                        batterySpec.batteryPolicy,
-                        engine,
-                        battery,
-                        batteryAggregator,
-                    )
-
-                carbonModel?.addReceiver(batteryPolicy)
-
-                FlowEdge(powerDistributor, batteryAggregator, ResourceType.POWER)
-
-                service.addBattery(battery)
-            } else {
-                FlowEdge(powerDistributor, simPowerSource, ResourceType.POWER)
-            }
+            // Create a battery and connect it to the powerSource
+            this.addBattery(engine, service, batterySpec, simPowerSource, powerDistributor, clusterName, carbonModel)
 
             // Create hosts, they are connected to the powerMux when SimMachine is created
-            for (hostSpec in hostSpecs) {
+            for ((name, type, _, model, cpuPowerModel, gpuPowerModel, embodiedCarbon, expectedLifetime) in hostSpecs) {
                 val simHost =
                     SimHost(
-                        hostSpec.name,
-                        hostSpec.type,
                         name,
+                        type,
+                        clusterName,
                         ctx.dispatcher.timeSource,
                         engine,
-                        hostSpec.model,
-                        hostSpec.cpuPowerModel,
-                        hostSpec.gpuPowerModel,
-                        hostSpec.embodiedCarbon,
-                        hostSpec.expectedLifetime,
+                        model,
+                        cpuPowerModel,
+                        gpuPowerModel,
+                        embodiedCarbon,
+                        expectedLifetime,
                         powerDistributor,
                     )
 
                 carbonModel?.addReceiver(simHost.simMachine?.psu)
 
-                require(simHosts.add(simHost)) { "Host with name ${hostSpec.name} already exists" }
+                require(simHosts.add(simHost)) { "Host with name $name already exists" }
                 service.addHost(simHost)
             }
         }
@@ -165,4 +110,106 @@ public class HostsProvisioningStep(
             }
         }
     }
+
+    private data class PowerSourceFlows(
+        val simPowerSource: SimPowerSource,
+        val powerDistributor: FlowDistributor,
+    )
+
+    private fun createSimPowerSource(
+        service: ComputeService,
+        engine: FlowEngine,
+        powerSourceSpec: PowerSourceSpec,
+        clusterName: String,
+        numHosts: Int,
+    ): PowerSourceFlows {
+        val simPowerSource = SimPowerSource(engine, powerSourceSpec.totalPower.toDouble(), powerSourceSpec.name, clusterName)
+        simPowerSources.add(simPowerSource)
+        service.addPowerSource(simPowerSource)
+
+        val powerDistributor =
+            FlowDistributorFactory.getFlowDistributor(
+                engine,
+                DistributionPolicy.MAX_MIN_FAIRNESS,
+                numHosts,
+                1,
+            )
+
+        return PowerSourceFlows(simPowerSource, powerDistributor)
+    }
+
+    private fun createCarbonModel(
+        ctx: ProvisioningContext,
+        engine: FlowEngine,
+        powerSourceSpec: PowerSourceSpec,
+        simPowerSource: SimPowerSource
+    ): CarbonModel? {
+        val carbonFragments = getCarbonFragments(powerSourceSpec.carbonTracePath)
+        var carbonModel: CarbonModel? = null
+        // Create Carbon Model
+        if (carbonFragments != null) {
+            carbonModel = CarbonModel(engine, carbonFragments, startTime)
+            carbonModel.addReceiver(simPowerSource)
+            ctx.registry.register(serviceDomain, CarbonModel::class.java, carbonModel)
+        }
+
+        return carbonModel;
+    }
+
+    private fun addBattery(
+        engine: FlowEngine,
+        service: ComputeService,
+        batterySpec: BatteryJSONSpec?,
+        simPowerSource: SimPowerSource,
+        powerDistributor: FlowDistributor,
+        clusterName: String,
+        carbonModel: CarbonModel?
+    ) {
+        if (batterySpec == null) {
+            FlowEdge(powerDistributor, simPowerSource, ResourceType.POWER)
+            return
+        }
+
+        // Create Battery Distributor
+        val batteryDistributor =
+            FlowDistributorFactory.getFlowDistributor(
+                engine,
+                DistributionPolicy.MAX_MIN_FAIRNESS,
+                2,
+                1,
+            )
+        FlowEdge(batteryDistributor, simPowerSource)
+
+        // Create Battery
+        val battery =
+            SimBattery(
+                engine,
+                batterySpec.capacity,
+                batterySpec.chargingSpeed,
+                batterySpec.initialCharge,
+                batterySpec.name,
+                clusterName,
+                batterySpec.embodiedCarbon,
+                batterySpec.expectedLifetime,
+            )
+        FlowEdge(battery, batteryDistributor)
+
+        // Create Aggregator
+        val batteryAggregator = BatteryAggregator(engine, battery, batteryDistributor)
+
+        val batteryPolicy =
+            createSimBatteryPolicy(
+                batterySpec.batteryPolicy,
+                engine,
+                battery,
+                batteryAggregator,
+            )
+
+        carbonModel?.addReceiver(batteryPolicy)
+
+        FlowEdge(powerDistributor, batteryAggregator, ResourceType.POWER)
+
+        service.addBattery(battery)
+    }
+
 }
